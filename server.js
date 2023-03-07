21bcc1a119402196a4e076764bfb36a5e97f6dc2
Hong Phong Le <phonghngle@gmail.com>
2023-03-07 20:39:09 +0700

    Buoi 1

--- server.js
+++ server.js
@@ -1,19 +1,30 @@
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-require('dotenv').config({ path: './env' });
-const port = process.env.PORT || 3000;
+const http = require('http');
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+const port = 3000;
+const host = 'localhost';
 
-let routes = require('./src/routes/routes'); //importing route
-routes(app);
+const server = http.createServer(function (req, res) {
+  const products = [
+    { id: 1, name: 'Product 1' },
+    { id: 2, name: 'Product 2' },
+  ];
 
-app.use(function (req, res) {
-  res.status(404).send({ url: req.originalUrl + ' not found' });
+  if (req.url == '/') {
+    res.setHeader('Content-Type', 'text/html');
+    res.end(/*html*/ `
+        <form action="/products" method="post">
+            <input type="text" placeholder="Product Name" name="productName" />
+            <button>Submit</button>
+        </form>`);
+  }
+
+  if (req.url == '/products') {
+    res.setHeader('Content-Type', 'application/json');
+    products.push({ id: 3, name: 'Product 3' });
+    res.end(JSON.stringify(products));
+  }
 });
 
-app.listen(port);
-
-console.log('RESTful API server started on: ' + port);
+server.listen(port, host, function () {
+  console.log(`Server is running http://${host}:${port}`);
+});
