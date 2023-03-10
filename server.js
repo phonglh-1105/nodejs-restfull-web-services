1329aa269f7540bf550534bcbc26e0b2d44b9bff
Hong Phong Le <phonghngle@gmail.com>
2023-03-10 12:50:52 +0700

    CRUD

--- server.js
+++ server.js
@@ -1,30 +1,60 @@
-const http = require('http');
+const products = [
+  { id: 1, name: 'Product 1' },
+  { id: 2, name: 'Product 2' },
+  { id: 3, name: 'Product 3' },
+];
 
-const port = 3000;
-const host = 'localhost';
+const express = require('express');
+require('dotenv').config({ path: '.env' });
 
-const server = http.createServer(function (req, res) {
-  const products = [
-    { id: 1, name: 'Product 1' },
-    { id: 2, name: 'Product 2' },
-  ];
+const port = process.env.PORT || 8080;
+const app = express();
 
-  if (req.url == '/') {
-    res.setHeader('Content-Type', 'text/html');
-    res.end(/*html*/ `
-        <form action="/products" method="post">
-            <input type="text" placeholder="Product Name" name="productName" />
-            <button>Submit</button>
-        </form>`);
-  }
-
-  if (req.url == '/products') {
-    res.setHeader('Content-Type', 'application/json');
-    products.push({ id: 3, name: 'Product 3' });
-    res.end(JSON.stringify(products));
-  }
+app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+app.get('/', function (req, res) {
+  res.send(/*html*/ `
+    <form action="" method="post">
+      <input type="text" placeholder="ProductName"/>
+      <button type="submit">Submit</button>
+    </form>
+  `);
 });
 
-server.listen(port, host, function () {
-  console.log(`Server is running http://${host}:${port}`);
+app.get('/products', function (req, res) {
+  res.json(products);
+});
+app.get('/products/:id', function (req, res) {
+  const id = req.params.id;
+  const product = products.find((p) => p.id == id);
+  res.json(product);
+});
+
+app.post('/products', function (req, res) {
+  const product = req.body;
+  products.push(product);
+  res.json(product);
+});
+app.delete('/products/:id', (req, res) => {
+  const id = req.params.id;
+  const product = products.filter((p) => p.id == id);
+  products.splice(products.indexOf(product), id);
+  res.json({
+    message: 'Product deleted',
+    products,
+  });
+});
+app.put('/products/:id', (req, res) => {
+  const id = req.params.id;
+  const body = req.body;
+  const newProducts = products.map((product) =>
+    product.id == id ? body : product,
+  );
+  res.status(200).json({
+    message: 'Product updated',
+    products: newProducts,
+  });
+});
+app.listen(port, function () {
+  console.log(`Server is running http://localhost:${port}`);
 });
