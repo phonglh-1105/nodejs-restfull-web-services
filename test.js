52ecbde1cfba3a4fc625c2b2305ad6e4cc6acc1c
Hong Phong Le <phonghngle@gmail.com>
2023-03-11 22:32:17 +0700

    caching_sha2_password:file test.js

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
   host: 'localhost',
   port: 3306,
-  user: 'root',
+  user: 'fpoly',
   password: 'phonglh-1105',
 });
 connection.connect(function (err) {
