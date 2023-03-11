8ff60584f97e3dc7e947d74f767a000a73e0c95c
Hong Phong Le <phonghngle@gmail.com>
2023-03-11 10:21:54 +0700

    caching_sha2_password:file test.js

--- test.js
+++ test.js
@@ -2,12 +2,10 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
   host: 'localhost',
   port: 3306,
-  user: 'phonglh',
-  password: 'phonglh',
-  database: 'test_db',
+  user: 'root',
+  password: 'phonglh-1105',
 });
-
-module.exports = connection;
-
-// sau khi hoàn tất thao tác, bạn có thể đóng kết nối với cơ sở dữ liệu như sau
-connection.end();
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log('Connected to MySQL database!');
+});
